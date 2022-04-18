09aa286bee76f7781b783fe6f972d03f9b667665
anlesmes17 <98337583+anlesmes17@users.noreply.github.com>
2022-04-18 14:33:17 -0500

    Update 000. Cabletica Final Table.sql

--- Cabltica/Master Table/000. Cabletica Final Table.sql
+++ Cabltica/Master Table/000. Cabletica Final Table.sql
@@ -10,12 +10,26 @@ Fixed_Base AS(
 
 ###################################################### FMC Match############################################################################################
 
+/*,FMC_Base AS (
+  SELECT DISTINCT Fixed_Month, Fixed_Account, B_Contrato, Mobile_Month 
+  FROM Fixed_Base Inner Join Mobile_Base 
+  ON Fixed_Month=Mobile_Month AND B_contrato=Fixed_Account
+)
 
+,FixedFMCMatch AS (
+  SELECT DISTINCT Fixed_Account, Fixed_Month, contrato, Fixed_Month
+  FROM Fixed_Base a LEFT JOIN FMC_Base b ON Fixed_Account=Contrato AND a.Fixed_Month=b.Fixed_Month
+)
+
+,MobileFMCMatch AS (
+  SELECT DISTINCT Mobile_Account, Mobile_Month, contrato, Mobile_Month
+  FROM Mobile_Base a LEFT JOIN FMC_Base b ON Mobile_Account=Contrato AND a.Mobile_Month=b.Mobile_Month
+)*/
 
 
 ############################################## Join Fixed Mobile ################################################
 
---,FullCustomerBase AS(
+,FullCustomerBase AS(
 SELECT DISTINCT
 CASE WHEN (Fixed_Account IS NOT NULL AND Mobile_Account IS NOT NULL) OR (Fixed_Account IS NOT NULL AND Mobile_Account IS NULL) THEN safe_cast(Fixed_Month as string)
       WHEN (Fixed_Account IS NULL AND Mobile_Account IS NOT NULL) THEN safe_cast(Mobile_Month as string)
@@ -39,8 +53,20 @@ WHEN (Mobile_Account IS NOT NULL AND Mobile_ActiveEOM=1 AND (ActiveEOM = 0 OR Ac
  END AS E_FMC_Status, f.*,m.*,
 FROM Fixed_Base f FULL OUTER JOIN Mobile_Base m 
 ON safe_cast(Fixed_Account as string)=B_Contrato AND Fixed_Month=Mobile_Month
---)
+)
 
+--,CustomerBase_FMCFlags AS(
+ 
+ SELECT t.*,
+ CASE 
+ WHEN (B_FMC_Status = "Fixed Only" OR (E_FMC_Status = "FMC soft")  AND (Mobile_ActiveBOM = 0 OR MOBILE_ACTIVEBOM IS NULL)) AND B_MIX = "1P" THEN "Fixed 1P"
+ WHEN (B_FMC_Status = "Fixed Only" OR (E_FMC_Status = "FMC soft")  AND (Mobile_ActiveBOM = 0 OR MOBILE_ACTIVEBOM IS NULL)) AND B_MIX = "2P" THEN "Fixed 2P"
+ WHEN (B_FMC_Status = "Fixed Only" OR (E_FMC_Status = "FMC soft")  AND (Mobile_ActiveBOM = 0 OR MOBILE_ACTIVEBOM IS NULL)) AND B_MIX = "3P" THEN "Fixed 3P"
+ WHEN (B_FMC_Status = "FMC soft") AND (ActiveBOM = 0 OR ActiveBOM is null) then "Mobile Only"
+ WHEN B_FMC_Status = "FMC soft" OR  B_FMC_Status = "Mobile Only" THEN B_FMC_Status
+ END AS B_FMCType,
+--Pendiente End of Month--
+ FROM FullCustomerBase t
 
 
 /*SELECT DISTINCT MONTH, E_FMC_Status, COUNT(*)
