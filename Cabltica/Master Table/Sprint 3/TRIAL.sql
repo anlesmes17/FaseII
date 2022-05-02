b5f777a962c429747d3a4aff2777c38825365274
anlesmes17 <98337583+anlesmes17@users.noreply.github.com>
2022-05-02 18:13:57 -0500

    Update TRIAL.sql

--- Cabltica/Master Table/Sprint 3/TRIAL.sql
+++ Cabltica/Master Table/Sprint 3/TRIAL.sql
@@ -156,10 +156,104 @@ SELECT RIGHT(CONCAT('0000000000',CONTRATO),10) AS CONTRATO, DATE_TRUNC(FECHA_APE
     GROUP BY CONTRATO, Call_Month
 )
 
---,BillingCallsMasterTable AS (
+,BillingCallsMasterTable AS (
 SELECT F.*, NumCalls
 FROM CallsMasterTable f LEFT JOIN CallsPerUser 
 ON safe_cast(CONTRATO AS string)=safe_cast(RIGHT(CONCAT('0000000000',Fixed_Account),10) AS string) AND safe_cast(Call_Month as string)=Month
---)
+)
 
-#########################################
+######################################### Soft dx first bill #####################################################
+
+
+,sales_gen AS (
+    SELECT DISTINCT ACT_ACCT_CD, MIN(ACT_ACCT_INST_DT) AS FECHA_INSTALACION
+    FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-04-20_Historical_CRM_ene_2021_mar_2022_D`
+    GROUP BY 1
+)
+
+,first_bill AS (
+    SELECT ACT_ACCT_CD, MIN(oldest_unpaid_bill_dt) AS f_bill
+    FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-04-20_Historical_CRM_ene_2021_mar_2022_D`
+    GROUP BY 1
+    ORDER BY 1
+)
+
+,clean_dna AS (
+    SELECT *, CASE WHEN LST_PYM_DT < PRIMER_OLDEST_UNPAID THEN NULL ELSE LST_PYM_DT END AS LAST_PAYMENT_DT
+    FROM (
+        SELECT *,
+            FIRST_VALUE(FECHA_EXTRACCION IGNORE NULLS) OVER (PARTITION BY ACT_ACCT_CD ORDER BY FECHA_EXTRACCION) AS PRIMER_DNA,
+            FIRST_VALUE(OLDEST_UNPAID_BILL_DT IGNORE NULLS) OVER (PARTITION BY ACT_ACCT_CD ORDER BY FECHA_EXTRACCION) AS PRIMER_OLDEST_UNPAID
+        FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-04-20_Historical_CRM_ene_2021_mar_2022_D`
+    )
+    WHERE ACT_ACCT_INST_DT >= '2021-01-01'
+)
+
+,first_bill_dna AS (
+    SELECT d.*
+    FROM clean_dna d
+    INNER JOIN first_bill f
+        ON (d.ACT_ACCT_CD = f.ACT_ACCT_CD AND d.oldest_unpaid_bill_dt = f.f_bill)
+    WHERE d.ACT_ACCT_INST_DT >= '2021-01-01'
+)
+
+,summary AS (
+    SELECT DISTINCT
+            act_acct_cd, 
+            min(ACT_ACCT_INST_DT) as act_cust_strt_dt,
+            DATE_TRUNC(min(ACT_ACCT_INST_DT),MONTH) AS MONTH_INSTALLATION,
+            max(oldest_unpaid_bill_dt) as oldest_unpaid_bill_dt,
+            max(cast (LAST_PAYMENT_DT as date) ) AS LAST_PAYMENT_DT,
+            min(fi_outst_age) as min_fi_outst_age, 
+            max(fi_outst_age) as max_fi_outst_age,
+            -- ARRAY_AGG(bundle_name IGNORE NULLS order by load_dt LIMIT 1)[OFFSET (0)]as bundle_name,
+            -- ARRAY_AGG(pd_vo_prod_nm IGNORE NULLS order by load_dt LIMIT 1)[OFFSET (0)]as pd_vo_prod_nm,
+            -- ARRAY_AGG(pd_tv_prod_nm IGNORE NULLS order by load_dt LIMIT 1)[OFFSET (0)]as pd_tv_prod_nm,
+            -- ARRAY_AGG(pd_bb_prod_nm IGNORE NULLS order by load_dt LIMIT 1)[OFFSET (0)]as pd_bb_prod_nm,
+            -- ARRAY_AGG(pd_mix_nm IGNORE NULLS order by load_dt LIMIT 1)[OFFSET (0)]as pd_mix_nm,
+            CASE WHEN (max(fi_outst_age) >=26 ) then 1 ELSE NULL END AS FIRST_BILL_REACHED_SOFT_DX,
+            CASE WHEN act_acct_cd IS NOT NULL THEN "New sale" ELSE NULL
+            END AS NewSales 
+    FROM first_bill_dna
+    GROUP BY 1
+    ORDER BY ACT_ACCT_CD
+    )
+    ,SOFT_DX_INSTALLS AS (
+    SELECT a.FECHA_INSTALACION, a.ACT_ACCT_CD, b.FIRST_BILL_REACHED_SOFT_DX FROM sales_gen a LEFT JOIN summary b 
+    ON a.FECHA_INSTALACION=b.act_cust_strt_dt AND a.ACT_ACCT_CD=b.ACT_ACCT_CD
+    )
+
+,SalesMasterTable AS (
+SELECT F.*, FIRST_BILL_REACHED_SOFT_DX,
+FROM BillingCallsMasterTable f LEFT JOIN SOFT_DX_INSTALLS s
+ON safe_cast(s.act_acct_cd AS string)=safe_cast(Fixed_Account AS string) AND DATE_TRUNC(safe_cast(s.FECHA_INSTALACION as date),MONTH)=safe_cast(Month as date)
+)
+
+############################################## Bill shocks #####################################################
+
+,AbsMRC AS (
+SELECT *, abs(mrc_change) AS Abs_MRC_Change FROM SalesMasterTable
+WHERE (B_FMC_Segment IN('P1_Fixed','P2','P3','P4') OR E_FMC_Segment IN('P1_Fixed','P2','P3','P4'))
+)
+,BillShocks AS (
+SELECT *,
+CASE
+WHEN Abs_MRC_Change>(TOTAL_B_MRC*(.05)) AND B_PLAN=E_PLAN THEN "Bill Schock" ELSE NULL END AS BillShocksKPI
+FROM AbsMRC
+)
+
+,BaseNoChanges AS (
+SELECT Month, COUNT(DISTINCT Fixed_Account) AS AccountsNoChanges FROM BillShocks
+GROUP BY 1
+)
+,CountBillShocks AS (
+  SELECT Month, COUNT(DISTINCT Fixed_Account) AS BillChanges 
+  FROM BillShocks
+  WHERE BillShocksKPI IS NOT NULL
+  GROUP BY 1
+)
+SELECT b.Month, AccountsNoChanges, BillChanges
+FROM BaseNoChanges b LEFT JOIN CountBillShocks c ON b.Month=c.Month
+ORDER BY Month
+
+########################################### Mounting Bills ######################################################
