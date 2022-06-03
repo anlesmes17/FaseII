5a2ea47602e7e6be89929a0d39590422fcefffca
anlesmes17 <98337583+anlesmes17@users.noreply.github.com>
2022-06-02 23:39:12 -0500

    Update 001. Sprint 4 KPI Table.sql

--- Cabltica/Master Table/Sprint 4/001. Sprint 4 KPI Table.sql
+++ Cabltica/Master Table/Sprint 4/001. Sprint 4 KPI Table.sql
@@ -8,11 +8,6 @@ SELECT * FROM `gcp-bia-tmps-vtr-dev-01.lla_temp_dna_tables.2022-04-18_Cabletica_
 
 ------------------------------------------------------- One Time & Repeated Callers -----------------------------------------------------------------
 
-,FECHAS AS (
-    SELECT *, LAST_DAY(month_start) AS month_end
-    FROM UNNEST(GENERATE_DATE_ARRAY(DATE('2021-01-01'), CURRENT_DATE(), INTERVAL 1 MONTH)) AS month_start
-)
-
 ,LLAMADAS AS(
     SELECT RIGHT(CONCAT('0000000000',CONTRATO),10) AS CONTRATO, FECHA_APERTURA AS FECHA_LLAMADA, DATE_TRUNC(FECHA_APERTURA,MONTH) AS MES_LLAMADA, TIQUETE_ID
     FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-01-12_CR_TIQUETES_SERVICIO_2021-01_A_2021-11_D`
@@ -78,3 +73,64 @@ SELECT * FROM `gcp-bia-tmps-vtr-dev-01.lla_temp_dna_tables.2022-04-18_Cabletica_
 ------------------------------------------------------------------ Users With Tech Tickets ----------------------------------------------------------------------
 
 
+,TIQUETES AS(
+    SELECT RIGHT(CONCAT('0000000000',CONTRATO),10) AS CONTRATO, FECHA_APERTURA AS FECHA_TIQUETE, DATE_TRUNC(FECHA_APERTURA,MONTH) AS MES_TIQUETE, TIQUETE
+    FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-01-19_CR_TIQUETES_AVERIAS_2021-01_A_2021-11_D`
+    WHERE 
+        CLASE IS NOT NULL AND MOTIVO IS NOT NULL AND CONTRATO IS NOT NULL
+        AND ESTADO <> "ANULADA"
+),
+
+TIQUETES_MES AS (
+    SELECT CONTRATO, MES_TIQUETE, COUNT(DISTINCT TIQUETE) AS N_TIQUETES_MES, MAX(FECHA_TIQUETE) AS MAX_APERTURA, DATE_SUB(MAX(FECHA_TIQUETE), INTERVAL 60 DAY) AS MAX_APERTURA_60D
+    FROM TIQUETES
+    GROUP BY 1,2
+    ORDER BY 2, 3 DESC
+),
+
+-- lo que hacemos aquí es buscar por cada mes los usuarios que tengan llamadas en los 60 días anteriores
+TIQUETES_60D AS (
+    SELECT m.*,l.FECHA_TIQUETE AS FECHA_TIQUETE_ANT, l.TIQUETE
+    FROM TIQUETES_MES AS m
+    LEFT JOIN TIQUETES AS l
+        ON ((m.CONTRATO = l.CONTRATO) AND (l.FECHA_TIQUETE BETWEEN m.MAX_APERTURA_60D AND m.MAX_APERTURA))
+),
+
+TIQUETES_GR AS (
+    SELECT MES_TIQUETE,CONTRATO,COUNT(DISTINCT TIQUETE) AS N_TIQUETES_60D,
+        CASE -- marca para el número de llamadas en los últimos 60 días
+            WHEN COUNT(DISTINCT TIQUETE) = 1 THEN '1'
+            WHEN COUNT(DISTINCT TIQUETE) >= 2 THEN '2+'
+        ELSE NULL END AS FLAG_TIQUETES_60D
+    FROM TIQUETES_60D
+    GROUP BY 1,2
+    ORDER BY 1, 3 DESC
+)
+
+,OneTicket AS (
+    SELECT MES_TIQUETE AS TICKET_MONTH, FLAG_TIQUETES_60D AS TICKETS_FLAG, Contrato AS ContratoTicket
+    FROM TIQUETES_GR
+    WHERE FLAG_TIQUETES_60D="1"
+    GROUP BY 1,2,3
+    ORDER BY 1
+)
+
+,MultipleTickets AS (
+    SELECT MES_TIQUETE AS TICKET_MONTH, FLAG_TIQUETES_60D AS TICKETS_FLAG,Contrato AS ContratoTicket
+    FROM TIQUETES_GR
+    WHERE FLAG_TIQUETES_60D="2+"
+    GROUP BY 1,2,3
+    ORDER BY 1
+)
+
+,OneTicketMasterTable AS(
+  SELECT f.*,ContratoTicket AS OneTicket
+  FROM MultipleCallsMasterTable f LEFT JOIN OneTicket ON safe_cast(ContratoTicket as string)=safe_cast(RIGHT(CONCAT('0000000000',Fixed_Account),10) as string) AND Month=safe_cast(TICKET_MONTH as string)
+)
+
+--,MultipleCallsMasterTable AS(
+  SELECT f.*,ContratoTicket AS MultipleTickets
+  FROM OneTicketMasterTable f LEFT JOIN MultipleTickets ON safe_cast(ContratoTicket as string)=safe_cast(RIGHT(CONCAT('0000000000',Fixed_Account),10) as string) AND Month=safe_cast(TICKET_MONTH as string)
+--)
+
+--------------------------------------------------------------- Customers With Failed Visits ------------------------------------------------------------------
