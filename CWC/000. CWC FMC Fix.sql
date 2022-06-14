e3cbca0d09737614492a556897758e5b1b6c2e98
anlesmes17 <98337583+anlesmes17@users.noreply.github.com>
2022-06-14 18:46:48 -0500

    Update 000. CWC FMC Fix.sql

--- CWC/000. CWC FMC Fix.sql
+++ CWC/000. CWC FMC Fix.sql
@@ -48,32 +48,35 @@ ELSE NULL END AS MarFMC
 FROM FMC_MissingMasterTable
 )
 
---,SegmentAndType AS(
+,SegmentAndType AS(
 Select *, 
 
 CASE WHEN MarFMC IS NOT NULL AND b_mixcode_adj IS NULL THEN 'Mobile Only'
 WHEN MarFMC IS NOT NULL THEN 'Soft/Hard FMC' 
-WHEN b_mixcode_adj IS NULL and Month=date('2022-03-01') THEN NULL ELSE b_FMCType END AS b_fmcTypeFinal,
+WHEN b_mixcode_adj IS NULL and Month=date('2022-03-01') and mobile_activebom<>1 THEN NULL ELSE b_FMCType END AS b_fmcTypeFinal,
 
 CASE WHEN MarFMC IS NOT NULL AND e_mixcode_adj IS NULL THEN 'Mobile Only'
 WHEN MarFMC IS NOT NULL THEN 'Soft/Hard FMC' 
-WHEN e_mixcode_adj IS NULL and Month=date('2022-03-01') THEN NULL ELSE e_FMCType END AS e_fmcTypeFinal,
+WHEN e_mixcode_adj IS NULL and Month=date('2022-03-01') and mobile_activebom<>1 THEN NULL ELSE e_FMCType END AS e_fmcTypeFinal,
 
 CASE 
 WHEN MarFMC IS NOT NULL AND b_mixcode_adj IS NULL THEN 'P1_Mobile'
-WHEN b_mixcode_adj IS NULL and Month=date('2022-03-01') THEN NULL
+WHEN b_mixcode_adj IS NULL and Month=date('2022-03-01') and mobile_activebom<>1 THEN NULL
 WHEN MarFMC IS NOT NULL and b_mixcode_adj ='1P' THEN 'P2'
 WHEN MarFMC IS NOT NULL and b_mixcode_adj ='2P' THEN 'P3'
 WHEN MarFMC IS NOT NULL and b_mixcode_adj ='3P' THEN 'P4'
 ELSE b_FMC_Segment END AS b_FMC_Segment_Final,
 CASE 
-WHEN MarFMC IS NOT NULL AND e_mixcode_adj IS NULL THEN 'P1_Mobile'
-WHEN e_mixcode_adj IS NULL and Month=date('2022-03-01') THEN NULL
+WHEN MarFMC IS NOT NULL AND e_mixcode_adj IS NULL and mobile_activeeom=1 THEN 'P1_Mobile'
+WHEN e_mixcode_adj IS NULL and Month=date('2022-03-01') and mobile_activeeom<>1 THEN NULL
 WHEN MarFMC IS NOT NULL and e_mixcode_adj ='1P' THEN 'P2'
 WHEN MarFMC IS NOT NULL and e_mixcode_adj ='2P' THEN 'P3'
 WHEN MarFMC IS NOT NULL and e_mixcode_adj ='3P' THEN 'P4'
 ELSE e_FMC_Segment END AS e_FMC_Segment_Final
 
 FROM FMC_MarFinal
---)
-
+)
+Select distinct Month, b_fmcTypeFinal,b_FMC_Segment_Final,e_fmcTypeFinal,e_FMC_Segment_Final,waterfall_flag, count(distinct final_account) FROM SegmentAndType
+WHERE Month>date('2022-01-01')
+group by 1,2,3,4,5,6
+order by 1,2,3,4,5,6
