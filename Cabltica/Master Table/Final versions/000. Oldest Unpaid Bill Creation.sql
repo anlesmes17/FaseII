5c86dbd0b9a2e595eec9b800689a8f0eccf0198a
anlesmes17 <98337583+anlesmes17@users.noreply.github.com>
2022-06-30 19:38:18 -0500

    Update 000. Oldest Unpaid Bill Creation.sql

--- Cabltica/Master Table/Final versions/000. Oldest Unpaid Bill Creation.sql
+++ Cabltica/Master Table/Final versions/000. Oldest Unpaid Bill Creation.sql
@@ -5,6 +5,7 @@ WITH
 FacturaCerrada as(
   SELECT DISTINCT FechaFactura,contrato,RIGHT(CONCAT('0000000000',factura) ,10) as factura,estado
   FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.20220628_Fact_Enca`
+  where estado ="A"
 )
 
 
@@ -15,14 +16,14 @@ FacturaCerrada as(
 )
 
 ,TEST as( --Esta Consulta une una factura con su fecha de pago
-  SELECT f.*,p.* FROM FacturaCerrada f LEFT JOIN PagoFactura p
+  SELECT DISTINCT f.*,p.* FROM FacturaCerrada f LEFT JOIN PagoFactura p
   ON safe_cast(factura as string)=fact_aplica
 )
 
 ################################################################# Unión Billing CRM ###############################################################################
 
 ,CRM as(
-  SELECT * FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-06-08_CR_HISTORIC_CRM_ENE_2021_MAY_2022`
+  SELECT DISTINCT * FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.2022-06-08_CR_HISTORIC_CRM_ENE_2021_MAY_2022`
 )
 
 ,Bill_and_Payment_Add AS( -- Esta consulta trae la fecha de la factura y la fecha de pago de la factura actual
@@ -30,6 +31,14 @@ FacturaCerrada as(
   FROM CRM f LEFT JOIN TEST t
   ON contrato=act_acct_cd AND Date_Trunc(FechaFactura,Month)=Date_Trunc(Fecha_Extraccion,Month)
 )
+
+SELECT * FROM Bill_and_Payment_Add
+Where act_acct_cd=84178
+order by Fecha_Extraccion
+
+SELECT DISTINCT FECHA_EXTRACCION, ACT_ACCT_CD,count(act_acct_cd) FROM Bill_and_Payment_Add
+group by 1,2
+order by 3 desc
 
 ,Last_Bill_Pym_Prel as( --Query para crear una tabla sólo con facturas únicas
   SELECT DISTINCT * except(estado) FROM TEST
@@ -48,8 +57,11 @@ FacturaCerrada as(
   order by 1
 )
 
+######################################################################## Mounting Bills ############################################################################################
+
+
 ,OldestPerpetuity as( --Consulta que trae la mímima factura de cada cliente que no ha sido pagada
-SELECT contrato,Min(FechaFactura) as OldestPerpetuity FROM TEST
+SELECT DISTINCT contrato,Min(FechaFactura) as OldestPerpetuity FROM TEST
 Where Fechapago is null
 group by 1
 )
@@ -59,21 +71,56 @@ group by 1
   ON f.contrato=b.contrato
 )
 
-,OldestPreliminary as(
-  SELECT f.*,t.*, FROM Last_Pym_CRM f LEFT JOIN PerpetuityTablaOldest t
+--,OldestPreliminary as(
+  SELECT DISTINCT f.*,t.*, FROM Last_Pym_CRM f LEFT JOIN PerpetuityTablaOldest t
   ON act_acct_cd=contrato AND Date_Trunc(FECHA_EXTRACCION,Month)=FacturaActual
-  where act_acct_cd=1126979
-  order by fecha_extraccion
+--)
+
+##################################################################### No Bill Emission ###############################################################################################
+
+,AllBillsCRM as( --Consulta que trae todas las facturas de cada cliente y las ata al CRM
+  SELECT f.act_acct_cd, f.Fecha_Extraccion,b.FechaFactura as BillNoEmission, FechaPago as PaymentNoBill
+  FROM CRM f LEFT JOIN TEST b
+  ON contrato=act_acct_cd
 )
 
---,OldestStepOne as(
-  SELECT f.*, CASE
+,OldestNoBillEmission as( --Consulta que crea una columna adicional para asignar Oldest a clientes que no le emitieron factura ese mes
+Select *, CASE
+WHEN Date_trunc(Fecha_Extraccion,Month)<>BillNoEmission AND Fecha_Extraccion>BillNoEmission AND Fecha_Extraccion<DATE(PaymentNoBill) THEN BillNoEmission
+ELSE NULL END AS OldestNoBill
+FROM AllBillsCRM
+)
+
+,AllBillsNoPayment as(
+Select * From OldestNoBillEmission WHERE OldestNoBill IS NOT NULL
+)
+
+--,BillsNoPaymentCRM as(
+  SELECT DISTINCT f.*,OldestNoBill  FROM OldestPreliminary f LEFT JOIN AllBillsNoPayment b
+  ON f.act_acct_cd=b.act_acct_cd AND f.Fecha_Extraccion=b.Fecha_extraccion
+--)
+
+
+,OldestStepOne as(
+  SELECT DISTINCT f.*, CASE
   WHEN Fecha_Extraccion>=DATE(Bill_Payment_Date) THEN NULL
   WHEN DATE(Bill_Payment_Date)>Fecha_Extraccion AND Payment_Prev_Bill IS NULL AND Prev_Bill IS NULL THEN Bill_Dt_M0
   WHEN Fecha_Extraccion<DATE(Bill_Payment_Date) AND Fecha_Extraccion<DATE(Payment_Prev_Bill) THEN Prev_Bill
   WHEN FECHA_EXTRACCION>=DATE(Payment_Prev_Bill) AND Fecha_Extraccion<=DATE(Bill_Payment_Date) THEN Bill_Dt_M0
-  WHEN Bill_Payment_Date IS NULL AND Payment_Prev_Bill IS NOT NUll THEN Prev_Bill
+  WHEN Bill_Payment_Date IS NULL AND Payment_Prev_Bill IS NOT NUll AND FECHA_EXTRACCION<DATE(Payment_Prev_Bill) THEN Prev_Bill
   WHEN Bill_DT_M0 IS NOT NULL THEN OldestPerpetuity
+  WHEN OldestNoBill IS NOT NULL AND Bill_DT_M0 IS NULL THEN OldestNoBill
   ELSE Null END AS OLDEST_UNPAID
-  FROM OldestPreliminary f
---)
+  FROM BillsNoPaymentCRM f
+)
+
+Select Distinct * except(Bill_Payment_Date,Payment_Prev_Bill,contrato,FechaPago,FacturaActual,Prev_Bill,OldestPerpetuity,OldestNoBill)
+FROM OldestStepOne
+
+
+/*
+SELECT DISTINCT FECHA_EXTRACCION, ACT_ACCT_CD,count(act_acct_cd) FROM OldestStepOne
+
+group by 1,2
+order by 3 desc
+*/
