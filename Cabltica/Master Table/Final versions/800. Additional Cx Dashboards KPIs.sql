7772f9397279ef3d9c07cdda4ec75959a60c75fa
anlesmes17 <98337583+anlesmes17@users.noreply.github.com>
2022-07-19 21:20:50 -0500

    Create 800. Additional Cx Dashboards KPIs.sql

---
+++ Cabltica/Master Table/Final versions/800. Additional Cx Dashboards KPIs.sql
@@ -0,0 +1,92 @@
+WITH
+
+fmc_table as(
+  Select *,
+  CONCAT(ifnull(B_Plan,""),ifnull(safe_cast(Mobile_ActiveBOM as string),"")) AS B_PLAN_ADJ, CONCAT(ifnull(E_Plan,""),ifnull(safe_cast(Mobile_ActiveEOM as string),"")) AS         
+  E_PLAN_ADJ  From `gcp-bia-tmps-vtr-dev-01.lla_temp_dna_tables.2022-07-14_Cabletica_FMC_DashboardInput`
+)
+
+,FinalTablePlanAdj AS (
+  SELECT DISTINCT *, 
+  CASE WHEN B_PLAN_ADJ=E_PLAN_ADJ THEN Fixed_Account ELSE NULL END AS no_plan_change_flag
+  FROM fmc_table
+)
+
+################################################################ Tech calls per 1k RGU ###############################################################################
+
+,Tiquetes as(
+  Select distinct Date_Trunc(Fecha_Apertura,Month) as TicketMonth,RIGHT(CONCAT('0000000000',CONTRATO),10) AS Contrato,count(distinct Tiquete) as TechCalls
+  From `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.20220623_CR_TIQUETES_AVERIA_2021-01_A_2022-05_D`
+  WHERE Clase is not null and Motivo is not null and Contrato is not null and estado <> "ANULADA"
+  group by 1,2
+)
+
+,Tiquetes_fmc_Table as(
+  Select f.*,TechCalls/ContractsFix as TechCall_Flag From FinalTablePlanAdj f left join Tiquetes
+  ON RIGHT(CONCAT('0000000000',Fixed_Account),10)=Contrato and Month=safe_cast(TicketMonth as string)
+)
+
+############################################################### Care Calls ##########################################################################################
+
+,Care_Calls as(
+  Select distinct Date_Trunc(Fecha_Apertura,Month) as CallMonth,RIGHT(CONCAT('0000000000',CONTRATO),10) AS Contrato,count(distinct Tiquete_ID) as CareCalls
+  From `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.20220623_CR_TIQUETES_SERVICIO_2021-01_A_2022-05_D`
+  where CLASE IS NOT NULL AND MOTIVO IS NOT NULL AND CONTRATO IS NOT NULL
+  and ESTADO <> "ANULADA" and TIPO <> "GESTION COBRO" --and MOTIVO <> "LLAMADA  CONSULTA DESINSTALACION" 
+  --AND subarea<>"VENTA VIRTUAL" AND subarea<>"FECHA Y HORA DE VISITA"and subarea<>"FECHA Y HORA DE VISITA WEB"
+  group by 1,2
+)
+
+,CareCalls_fmc_Table as (
+  Select f.*,CareCalls/ContractsFix as CareCall_Flag From Tiquetes_fmc_Table f left join Care_Calls
+  ON RIGHT(CONCAT('0000000000',Fixed_Account),10)=Contrato and Month=safe_cast(CallMonth as string)
+)
+
+
+########################################################## Billing Calls per Bill Variation ######################################################################
+
+,AbsMRC AS (
+SELECT *, abs(mrc_change) AS Abs_MRC_Change FROM CareCalls_fmc_Table
+
+)
+,BillVariations AS (
+SELECT DISTINCT *,
+CASE
+WHEN Abs_MRC_Change>(TOTAL_B_MRC*(.05)) AND B_PLAN=E_PLAN AND no_plan_change_flag is not null THEN RIGHT(CONCAT('0000000000',Fixed_Account),10) ELSE NULL END AS BillVariation_flag
+FROM AbsMRC
+)
+
+,BillVariation_MasterTable as(
+  Select distinct f.*,BillVariation_Flag From CareCalls_fmc_Table f left join BillVariations b
+  ON f.Fixed_Account=b.Fixed_Account and f.Month=b.Month
+)
+
+,BillVariation_Calls as(
+  Select distinct Date_Trunc(Fecha_Apertura,Month) as CallMonth,RIGHT(CONCAT('0000000000',CONTRATO),10) AS Contrato,count(distinct Tiquete_ID) as BillingCalls
+  From `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.20220623_CR_TIQUETES_SERVICIO_2021-01_A_2022-05_D`
+  WHERE 
+  CLASE IS NOT NULL AND MOTIVO IS NOT NULL AND CONTRATO IS NOT NULL
+  AND ESTADO <> "ANULADA" AND TIPO <> "GESTION COBRO" AND MOTIVO = "CONSULTAS DE FACTURACION O COBRO" 
+  group by 1,2
+)
+
+,BillVariationCalls_MasterTable as(
+  Select f.*,BillingCalls From BillVariation_MasterTable f left join BillVariation_Calls
+  ON Contrato=RIGHT(CONCAT('0000000000',Fixed_Account),10) and Month=safe_cast(CallMonth as string)
+)
+
+
+
+############################################################## Grouped Table ##########################################################################################
+
+Select Distinct Month,round(sum(B_NumRGUs/ContractsFix),0) as FixedRGUs,
+round(sum(TechCall_Flag),0) as TechCalls,
+round(sum(TechCall_Flag)*1000/sum(B_NumRGUs),0) as TechCallsPer1kRGU,
+round(sum(CareCall_Flag),0) as CareCalls,
+round(sum(CareCall_Flag)*1000/sum(B_NumRGUs),0) as CareCallsPer1kRGU,
+Count(distinct BillVariation_Flag) as BillVariations,
+sum(BillingCalls) as BillingCalls,
+round(sum(BillingCalls)/Count(distinct BillVariation_Flag),3) as BillingCallsPerBillVariation
+From BillVariationCalls_MasterTable
+group by 1
+order by 1
